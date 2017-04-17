4440af3ccd4d42364fb03a7b938fda1324712d73
Simon Foster <simon@Simons-MBP.lan>
2017-04-17 22:42:50 +0100

    Updated styling and added event details.

--- facebook-toolbox.php
+++ facebook-toolbox.php
@@ -3,7 +3,7 @@
 Plugin Name: Facebook Toolbox
 Plugin URI: http://wwww.simonfoster.co.uk/facebook-toolbox
 Description: Provides various snippets for integrating Facebook in Wordpress posts
-Version: 0.01
+Version: 0.02
 Author: Simon Foster
 Author URI: http://www.simonfoster.co.uk
 */
@@ -45,6 +45,7 @@ add_action('wp_enqueue_scripts', 'fbt_add_stylesheet', 99999);
 /** tell wordpress to register the shortcode */
 add_shortcode('fbt-text', 'fbt_text_handler');
 add_shortcode('fbt-status', 'fbt_status_handler');
+add_shortcode('fbt-event', 'fbt_event_handler');
 
 
 /**
@@ -53,18 +54,31 @@ add_shortcode('fbt-status', 'fbt_status_handler');
  * Send a query to the Facebook Graph API and return the response as a PHP object.
  *
  * @param string $url The query to send to Facebook.
+ * @param strign $params An extra paramaters to pass in the URL, appended to the url i.e. &forward=false
  *
  * @return object The response from Facebook, using json_encode to change it to an object.
  */
-function fbt_api($url)
+function fbt_api($url, $params = '')
 {
+	$url = 'https://graph.facebook.com/v2.8/' . $url . '?access_token=' . FBT_APP_ID . '|' . FBT_APP_SECRET . $params;
+
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($curl, CURLOPT_VERBOSE, 1);
-	curl_setopt($curl, CURLOPT_URL, 'https://graph.facebook.com/v2.7/' . $url . '?access_token=' . FBT_APP_ID . '|' . FBT_APP_SECRET);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($curl, CURLOPT_VERBOSE, 0);
+	curl_setopt($curl, CURLOPT_URL, $url);
 
-	return json_decode(curl_exec($curl));
+	$data = curl_exec($curl);
+
+	if (FBT_DEBUG=='yes') {
+		error_log($url);
+		error_log(' = ' . $data);
+	}
+
+	curl_close($curl);
+
+	return json_decode($data);
 }
 
 
@@ -104,42 +118,126 @@ function fbt_status_handler($arrParameters)
 	$likes = fbt_api($arrParameters['post'] . '/likes');
 	$comments = fbt_api($arrParameters['post'] . '/comments');
 
-	$summary = count($likes->data) . ' likes ' . count($comments->data) . ' comments';
+	$summary = count($likes->data) . ' like' . (count($likes->data)==1 ? '' : 's') . ' &bull; ' . count($comments->data) . ' comment' . (count($comments->data)==1 ? '' : 's');
 
 	// depending on how many likes we have, express the names as text
 	if (count($likes->data)>0) {
 		switch (count($likes->data)) {
 			case 1: 
-				$detail = $likes->data[0]->name . ' likes this';
+				$detail = $likes->data[0]->name . ' likes this.';
 				break;
 
 			case 2:
-				$detail = $likes->data[0]->name . ' and ' . $likes->data[1]->name . ' like this';
+				$detail = $likes->data[0]->name . ' and ' . $likes->data[1]->name . ' like this.';
 				break;
 
 			case 3:
-				$detail = $likes->data[0]->name . ', ' . $likes->data[1]->name . ' and ' . $likes->data[2]->name . ' like this';
+				$detail = $likes->data[0]->name . ', ' . $likes->data[1]->name . ' and ' . $likes->data[2]->name . ' like this.';
 				break;
 
 			default:
-				$detail = $likes->data[0]->name . ', ' . $likes->data[1]->name . ' and ' . (count($likes->data) - 2) . ' others like this';
+				$detail = $likes->data[0]->name . ', ' . $likes->data[1]->name . ' and ' . (count($likes->data) - 2) . ' others like this.';
 		}
 
-		$detail = '<p>' . $detail . '</p>';
+		$detail = '<div class="fbt_likes">' . $detail . '</div>';
 	}
 
 	/** depending on the comments, express the detail */
 	if (count($comments->data)>0) {
 		foreach ($comments->data as $comment) {
-			$detail .= '<hr>';
-			$detail .= '<p>' . $comment->from->name . '<br>' . $comment->message . '</p>';
+//			$detail .= '<hr>';
+			$detail .= '<div class="fbt_comment">';
+			$picture = fbt_api($comment->from->id . '/picture', '&redirect=0');
+//			$detail .= '(' . $picture->data->url . ')';
+			if (!empty($picture->data->url)) {
+				$detail .= '<a href="https://www.facebook.com/' . $comment->from->id . '" target="_blank"><img src="' . $picture->data->url . '" width="27"></a>';
+			}
+			$detail .= '<span><b>' . $comment->from->name . '</b> ' . $comment->message . '<br>' . date('jS F Y', strtotime($comment->created_time)) . '</span></div>';
 		}
 	}
 
-	$ret = '<div id="click_' . $arrParameters['post'] . '" class="fbt_summary fbt_smaller"><p>' . $summary . '</p></div>';
+	$ret = '<div class="fbt_container">';
+	$ret .= '<div id="click_' . $arrParameters['post'] . '" class="fbt_summary fbt_smaller">';
+	$ret .= '<a href="https://www.facebook.com/' . $arrParameters['post'] . '" target="_blank">';
+	$ret .= '<img src="' . plugins_url( 'FB-f-Logo__blue_29.png', __FILE__ ) . '" >';
+	$ret .= '</a>';
+	$ret .= '<span>' . $summary . '</span>';
+	$ret .= '</div>';
 	$ret .= '<div id="detail_' . $arrParameters['post'] . '" class="fbt_hidden fbt_smaller">' . $detail . '</div>';
+	$ret .= '</div>';
 	$ret .= '<script>jQuery(\'#click_' . $arrParameters['post'] . '\').on(\'click\', function() {';
 	$ret .= ' jQuery(\'#detail_' . $arrParameters['post'] . '\').slideToggle(); ';
+	$ret .= '}); </script>';
+
+	return $ret;
+}
+
+
+/**
+ * The handler for [fbt-event event=<id>] shortcode
+ *
+ * Access a Facebook event and retrieve the Attendees and Comments, outputting a summary plus detail
+ * detail in a hidden div which can be displayed by clicking the summary.
+ *
+ * @param array $arrParameters A list of options within the shortcode,namely the event to query.
+ *
+ * @return string The data retrieved from Facebook.
+ */
+function fbt_event_handler($arrParameters)
+{
+	$attending = fbt_api($arrParameters['event'] . '/attending');
+	$interested = fbt_api($arrParameters['event'] . '/interested');
+	$comments = fbt_api($arrParameters['event'] . '/comments');
+
+	$summary = count($attending->data) . ' attending &bull; ' . count($interested->data) . ' interested';
+
+	// depending on how many likes we have, express the names as text
+	if (count($attending->data)>0) {
+		switch (count($attending->data)) {
+			case 1: 
+				$detail = $attending->data[0]->name . ' is attending.';
+				break;
+
+			case 2:
+				$detail = $attending->data[0]->name . ' and ' . $attending->data[1]->name . ' are attending.';
+				break;
+
+			case 3:
+				$detail = $attending->data[0]->name . ', ' . $attending->data[1]->name . ' and ' . $attending->data[2]->name . ' are attending.';
+				break;
+
+			default:
+				$detail = $attending->data[0]->name . ', ' . $attending->data[1]->name . ' and ' . (count($attending->data) - 2) . ' others are attending.';
+		}
+
+		$detail = '<div class="fbt_likes">' . $detail . '</div>';
+	}
+
+	/** depending on the comments, express the detail */
+	if (count($comments->data)>0) {
+		foreach ($comments->data as $comment) {
+//			$detail .= '<hr>';
+			$detail .= '<div class="fbt_comment">';
+			$picture = fbt_api($comment->from->id . '/picture', '&redirect=0');
+//			$detail .= '(' . $picture->data->url . ')';
+			if (!empty($picture->data->url)) {
+				$detail .= '<a href="https://www.facebook.com/' . $comment->from->id . '" target="_blank"><img src="' . $picture->data->url . '" width="27"></a>';
+			}
+			$detail .= '<span><b>' . $comment->from->name . '</b> ' . $comment->message . '<br>' . date('jS F Y', strtotime($comment->created_time)) . '</span></div>';
+		}
+	}
+
+	$ret = '<div class="fbt_container">';
+	$ret .= '<div id="click_' . $arrParameters['event'] . '" class="fbt_summary fbt_smaller">';
+	$ret .= '<a href="https://www.facebook.com/' . $arrParameters['event'] . '" target="_blank">';
+	$ret .= '<img src="' . plugins_url( 'FB-f-Logo__blue_29.png', __FILE__ ) . '" >';
+	$ret .= '</a>';
+	$ret .= '<span>' . $summary . '</span>';
+	$ret .= '</div>';
+	$ret .= '<div id="detail_' . $arrParameters['event'] . '" class="fbt_hidden fbt_smaller">' . $detail . '</div>';
+	$ret .= '</div>';
+	$ret .= '<script>jQuery(\'#click_' . $arrParameters['event'] . '\').on(\'click\', function() {';
+	$ret .= ' jQuery(\'#detail_' . $arrParameters['event'] . '\').slideToggle(); ';
 	$ret .= '}); </script>';
 
 	return $ret;
